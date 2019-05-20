1ecb68591ed60955550c7f874be5298b9acd44f5
Marissa Kawinski <marissakawinski@gmail.com>
2019-05-20 13:47:38 -0400

    Lots of changes!  Matches section 8 lecture 54

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '..', 'public')
-const viewsPath = path.join(__dirname, '..', 'templates')
+const publicDirPath = path.join(__dirname, '..', 'public')
+const viewsPath = path.join(__dirname, '..', 'templates', 'views')
+const partialsPath = path.join(__dirname, '..', 'templates', 'partials')
 
-// Set up handlebars engine and views location
+// Set up handlebars engine and views/partials location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Set up static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirPath))
 
 app.get('', (req, res) => {
    res.render('index', {
-      title: 'Weather App',
+      title: 'Forecast',
       name: 'Marissa Kawinski'
    })
 })
@@ -31,6 +34,7 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
    res.render('help', {
       title: 'Help',
+      name:'Marissa Kawinski',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
    })
 })
@@ -42,11 +46,39 @@ app.get('/forecast', (req, res) => {
    })
 })
 
+app.get('/products', (req, res) => {
+   if(!req.query.search) {
+      return res.send({
+         error: 'You must provide a search term'
+      })
+   }
+   console.log(req.query)
+   res.send({
+      products: []
+   })
+})
+
 app.get('/time-machine', (req, res) => {
    res.send({
       location: 'Meadville',
       date: '05/18/2017',
       forecast: 'Sunny with a high of 76F'
+   })
+})
+
+app.get('/help/*', (req, res) => {
+   res.render('404', {
+      title: 404,
+      name: 'Marissa Kawinski',
+      errorMessage: 'Help article not found.'
+   })
+})
+
+app.get('*', (req, res) => {
+   res.render('404', {
+      title: 404,
+      name: 'Marissa Kawinski',
+      errorMessage: 'Page not found.'
    })
 })
 
