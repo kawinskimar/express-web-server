e10a281898a20da1255f39d113c889d2c215bac0
Marissa Kawinski <marissakawinski@gmail.com>
2019-05-20 10:35:22 -0400

    Add handlebars and remove static HTML files

--- src/app.js
+++ src/app.js
@@ -3,13 +3,36 @@ const express = require('express')
 
 const app = express()
 
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '..', 'public')
+const viewsPath = path.join(__dirname, '..', 'templates')
 
-app.use(express.static(publicDirectoryPath))
+// Set up handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Set up static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-   res.render('index')
+   res.render('index', {
+      title: 'Weather App',
+      name: 'Marissa Kawinski'
+   })
+})
+
+app.get('/about', (req, res) => {
+   res.render('about', {
+      title: 'About',
+      name: 'Marissa Kawinski'
+   })
+})
+
+app.get('/help', (req, res) => {
+   res.render('help', {
+      title: 'Help',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+   })
 })
 
 app.get('/forecast', (req, res) => {
